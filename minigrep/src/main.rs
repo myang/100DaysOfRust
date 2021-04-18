c099be43380ca70bf0a7a5dc5ed6fd7d38b518bf
myang <mu.yang@gmail.com>
2021-04-18 23:37:07 +0300

    arg handling.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 }
