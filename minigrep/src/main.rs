38af9576640a406f8885cb75a6db0336bd1331c0
myang <mu.yang@gmail.com>
2021-04-21 23:46:12 +0300

    refactor command line handing.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,15 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
+    let (query, filename) = parse_query(&args);
+
+}
+
+fn parse_query(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let filename = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("Sth went wrong reading the file");
-
-    println!("with text:\n{}", contents);
-    
+    (query, filename)
 }
