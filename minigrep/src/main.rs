32e91dc405d8d87c76454e176bff3f694c94876c
myang <mu.yang@gmail.com>
2021-04-22 15:53:25 +0300

    add error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,26 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let (query, filename) = parse_query(&args);
+    let config = Config::new(&args);
 
+    /*let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+        */
 }
 
-fn parse_query(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
