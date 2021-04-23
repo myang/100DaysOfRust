26a735864b46970dc9ad4eb04e123a7888732dcf
myang <mu.yang@gmail.com>
2021-04-23 17:45:19 +0300

    split logic to lib.rs.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,30 +1,21 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    /*let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-        */
-}
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
