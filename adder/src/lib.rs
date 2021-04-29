9350947bc786147cb986cd32e16e2d91b5e4029e
myang <mu.yang@gmail.com>
2021-04-29 21:28:31 +0300

    add more test cases.

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,12 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("make this test fail;")
     }
 }
