74bb47f65b920109d125a463a66322328f57a53c
Mu Yang <muyang@EY-Mac.local>
2021-05-01 00:05:36 +0300

    update Config to use iterator.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -14,13 +14,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'str> {
+        args.next();
 
-        Ok(Config { query, filename })
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+
+        Ok(Config { 
+            query, 
+            filename,
+            case_sensitive,
+        })
     }
 }
