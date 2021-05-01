1a49c64bfd276966a9b587b58fca57dd1d380185
minusta <mu.yang@gmail.com>
2021-05-01 23:29:58 +0300

    add search.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,13 +2,6 @@ use std::error::Error;
 use std::fs;
 use std::env;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -36,4 +29,41 @@ impl Config {
             case_sensitive,
         })
     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    }
+    else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
