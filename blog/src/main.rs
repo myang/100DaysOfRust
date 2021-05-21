3390c889ffc7c83c2316cf93894404f01328ce1e
minusta <mu.yang@gmail.com>
2021-05-22 00:06:12 +0300

    update main.rs

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,3 +1,14 @@
+use blog::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    //post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
