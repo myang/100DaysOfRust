249fe626b0f0b4df80f66f49a4812a0ec10e0236
minusta <mu.yang@gmail.com>
2021-06-12 17:38:39 +0300

    add move.

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -2,15 +2,20 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
         for i in 1..10 {
-            println!("thread {}", i);
+            println!("vector: {:?}", v);
             thread::sleep(Duration::from_millis(1));
         }
     });
+
+    handle.join().unwrap();
 
     for i in 1..5 {
         println!("main {}", i);
         thread::sleep(Duration::from_millis(1));
     }
+
 }
