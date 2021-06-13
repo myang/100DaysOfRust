2543abef7dfe551e49f3aa8284af75192e4b55ac
minusta <mu.yang@gmail.com>
2021-06-13 19:02:19 +0300

    add multi-producers and simgle consumer.

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -3,27 +3,38 @@ use std::time::Duration;
 use std::sync::mpsc;
 
 fn main() {
-    let v = vec![1, 2, 3];
     let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
 
-    let handle = thread::spawn(move || {
-        for _ in 1..10 {
-            println!("vector: {:?}", v);
-            thread::sleep(Duration::from_millis(1));
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
-
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    handle.join().unwrap();
+    //handle.join().unwrap();
 
-    for i in 1..5 {
-        println!("main {}", i);
-        thread::sleep(Duration::from_millis(1));
+    for received in rx {
+        println!("Got: {}", received);
     }
-
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
 
 }
