13496db761146f8d47b1efdb9f93324cc49a456a
minusta <mu.yang@gmail.com>
2021-06-21 17:02:17 +0300

    add mpsc

--- web-server/src/lib.rs
+++ web-server/src/lib.rs
@@ -1,7 +1,14 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -14,18 +21,35 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for id in 0..size {
             // create some threads and store them in the vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool {threads}
+        ThreadPool {workers, sender}
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+
+        Worker {id, thread}
     }
 }
